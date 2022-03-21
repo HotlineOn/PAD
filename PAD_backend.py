f6607cdbddc2764f81b187d1f7e02f10af1a4a48
nopalm <hotlixon@gmail.com>
2022-03-21 18:01:01 +0300

    Отбор случайных страниц со статьями

--- PAD_backend.py
+++ PAD_backend.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 import pickle
 import yaml
 from bs4 import BeautifulSoup
@@ -18,10 +20,6 @@ NOT_IAD_OPTIONS = [
 ]
 
 
-def get_random_articles(articles: list, num_of_articles: int) -> list:
-    return random.sample(articles, num_of_articles)
-
-
 def get_config() -> dict:
     if os.path.exists('cfg.yaml'):
         with open('cfg.yaml') as f:
@@ -34,86 +32,113 @@ def set_config(dictionary: dict) -> None:
         yaml.dump(dictionary, f)
 
 
-def make_urls(is_iad: bool, articles_per_page: int, num_of_pages: int) -> list:
-    """Синтез url адресов"""
-    search_address = 'https://arxiv.org/search/cs?query='
-    search_option = '&searchtype=all&abstracts=show&order=-announced_date_first&size=' + \
-                    str(articles_per_page) + '&date-date_type=submitted_date'
-    query = IAD_OPTIONS if is_iad else NOT_IAD_OPTIONS
-    urls = list()
-    for option in query:
-        for page in range(num_of_pages):
-            search_start = '&start=' + str(page * articles_per_page)
-            urls.append(search_address + option + search_option + search_start)
-    return urls
-
-
 def delete_file(file_name: str) -> None:
     """Удаление файла(для отладки)"""
     os.remove(file_name)
 
 
-def dump_binary(file_name: str, data: list) -> None:
+def dump_binary(data: dict, file_name: str) -> None:
     """Загрузка в бинарный файл"""
     with open(file_name, 'wb') as bin_file_write:
         pickle.dump(data, bin_file_write)
 
 
-def load_binary(file_name: str) -> list:
+def load_binary(file_name: str) -> dict:
     """Загрузка из бинарного файла"""
     with open(file_name, 'rb') as bin_file_read:
         return pickle.load(bin_file_read)
 
 
-def download(urls: list, file_name: str) -> None:
-    """Скачивание с сайта в файл"""
+def download_page(url: str) -> str:
+    """Скачивание html кода сайтов"""
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'}
-    htmls = list()
-    for url in urls:
-        response = requests.get(url, headers=headers)
-        assert response.status_code == 200, 'Нет соединения с сервером'
-        htmls.append(response.text)
-    dump_binary(file_name, htmls)
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('https://', adapter)
+    response = session.get(url, headers=headers)
+    assert response.status_code != 404, 'Неправильный url запрос'
+    assert response.status_code != 400, 'Немного неправильный url запрос'
+    assert response.status_code == 200, 'Нет соединения с сервером: Ошибка ' + str(response.status_code)
+
+    return response.text
 
 
-def parse(file_name: str) -> list:
-    """Парсинг загруженных из файла страниц"""
-    articles = []
-    htmls = load_binary(file_name)
-    for html in htmls:
+def download_all_htmls(is_iad: bool, articles_per_page: int, num_of_pages: int) -> dict:
+    """Скачивание рандомных страниц """
+    search_address = 'https://arxiv.org/search/cs?searchtype=all&query='
+    search_option = '&abstracts=show&order=-submitted_date&size=' + \
+                    str(articles_per_page) + '&date-date_type=submitted_date'
+    query_options = IAD_OPTIONS if is_iad else NOT_IAD_OPTIONS
+    all_htmls = dict()
+    for search_key_words in query_options:
+        html = download_page(search_address + search_key_words + search_option + '&start=0')
         soup = BeautifulSoup(html, 'lxml')
-        for article in soup.find_all('li', {'class': 'arxiv-result'}):
-            article_url = article.find('p', {'class': 'list-title'}).a['href']
-            assert len(article_url) > 0, "url не распознано"
-            title_html = article.find('p', {'class': 'title'})
-            assert len(title_html) > 0, "Название статьи не распознано"
-            title = title_html.text.strip()
-            tags_html = article.find('div', {'class': 'tags'}).find_all('span', {'class': 'tag'})
-            assert len(tags_html) > 0, "Теги не распознаны"
-            tag_codes = []
-            tag_names = []
-            for tag in tags_html:
-                try:
+        total_num_of_articles_html = soup.find('h1', {'class': 'title'})
+        assert len(total_num_of_articles_html) > 0, "Общее количество статей не распознано"
+        total_num_of_articles = int(total_num_of_articles_html.text.split()[3].replace(',', '').replace(';', ''))
+        if total_num_of_articles > 10000:
+            total_num_of_articles = 10000
+
+        pages_list = list(range(int(total_num_of_articles / articles_per_page - 1)))
+        random_pages = random.sample(pages_list, num_of_pages + 2)  # Рандомные страницы
+        query_htmls = list()
+        for page in random_pages:
+            search_start = '&start=' + str(page * articles_per_page)
+            query_htmls.append(download_page(search_address + search_key_words + search_option + search_start))
+        all_htmls[search_key_words] = query_htmls
+    return all_htmls
+
+
+def parse(is_iad: bool, all_htmls: dict, articles_per_query: int) -> dict:
+    """Парсинг загруженных из файла страниц"""
+    all_articles_structured = dict()
+    iad_dict_articles = dict()
+    all_articles = list()
+    if not is_iad:
+        if os.path.exists('IAD.bin'):
+            iad_dict_articles = load_binary('IAD.bin')
+        else:
+            return {}
+    iad_articles = [article for query in iad_dict_articles.values() for article in query]
+
+    for key, query_htmls in all_htmls.items():
+        articles = list()
+        for html in query_htmls:
+            soup = BeautifulSoup(html, 'lxml')
+            for article in soup.find_all('li', {'class': 'arxiv-result'}):
+
+                article_url = article.find('p', {'class': 'list-title'}).a['href']
+                assert len(article_url) > 0, "url не распознано"
+
+                title_html = article.find('p', {'class': 'title'})
+                assert len(title_html) > 0, "Название статьи не распознано"
+                title = title_html.text.strip()
+
+                tags_html = article.find('div', {'class': 'tags'}).find_all('span', {'class': 'tag'})
+                assert len(tags_html) > 0, "Теги не распознаны"
+                tag_codes = list()
+                tag_names = list()
+                for tag in tags_html:
                     tag_names.append(tag['data-tooltip'])
                     tag_codes.append(tag.text)
-                except KeyError:
-                    pass
-            assert len(tag_codes) == len(tag_names), 'Длины списка кодов и списка имён тегов не совпадает' + \
-                                                     str(len(tag_codes)) + ' ' + str(len(tag_names))
-            authors_html = article.find('p', {'class': 'authors'}).find_all('a')
-            assert len(authors_html) > 0, "Имена авторов не распознаны"
-            authors = []
-            for author in authors_html:
-                authors.append(author.text)
-            # Текст без кнопки Less
-            abstract_html = article.find('span', {'class': 'abstract-full'}).text
-            assert len(abstract_html) > 0, "Описание текста статьи не распознано"
-            abstract = abstract_html.strip().split('\n')[0]
-            submission_date_html = article.find('p', {'class': 'is-size-7'})
-            assert len(submission_date_html) > 0, "Дата публикации не распознана"
-            submission_date = submission_date_html.contents[1].strip()[:-1]
-            articles.append(
-                {
+
+                authors_html = article.find('p', {'class': 'authors'}).find_all('a')
+                assert len(authors_html) > 0, "Имена авторов не распознаны"
+                authors = list()
+                for author in authors_html:
+                    authors.append(author.text)
+
+                # Текст без кнопки Less
+                abstract_html = article.find('span', {'class': 'abstract-full'})
+                assert len(abstract_html) > 0, "Описание текста статьи не распознано"
+                abstract = abstract_html.text.strip().split('\n')[0]
+
+                submission_date_html = article.find('p', {'class': 'is-size-7'})
+                assert len(submission_date_html) > 0, "Дата публикации не распознана"
+                submission_date = submission_date_html.contents[1].strip()[:-1]
+
+                article = {
                     'url': article_url,
                     'title': title,
                     'tags':
@@ -125,5 +150,11 @@ def parse(file_name: str) -> list:
                     'abstract': abstract,
                     'submitted': submission_date
                 }
-            )
-    return articles
+                if article not in all_articles:
+                    if article not in iad_articles:
+                        articles.append(article)
+                        all_articles.append(article)
+
+        all_articles_structured[key] = random.sample(articles, articles_per_query)
+
+    return all_articles_structured
--- main.py
+++ main.py
@@ -9,13 +9,15 @@ if config:
 
 
 def articles_window(path_file_articles: str, preview_num: int) -> None:
-    articles = get_random_articles(load_binary(path_file_articles), preview_num)
+    dict_articles = load_binary(path_file_articles)
+    random_articles = random.sample([article for query in dict_articles.values() for article in query], preview_num)
     output_text = ''
-    for article in articles:
+    for article in random_articles:
         output_text += 'Название: ' + article['title'] + '\n' + \
                        'Темы: ' + ', '.join(article['tags']['names']) + '\n' + \
                        'Описание: ' + article['abstract'] + '\n' + \
                        'Дата публикации: ' + article['submitted'] + '\n\n\n'
+
     preview_layout = [
         [sg.Multiline(output_text, size=(100, 30), disabled=True)],
         [sg.Submit('ОК')]
@@ -23,12 +25,9 @@ def articles_window(path_file_articles: str, preview_num: int) -> None:
 
     preview = sg.Window('Предпросмотр Статей', preview_layout)
 
-    while True:
-        event, _ = preview.read()
-        if event == 'ОК' or event == sg.WIN_CLOSED:
-            break
-
-    preview.close()
+    event, _ = preview.read()
+    if event == 'ОК' or event == sg.WIN_CLOSED:
+        preview.close()
 
 
 def options_window() -> None:
@@ -37,8 +36,8 @@ def options_window() -> None:
             sg.Column([
                 [sg.Text('Какие данные:')],
                 [
-                    sg.Radio('ИАД', 'RADIO1', default=True, key='-IS IAD-'),
-                    sg.Radio('не ИАД', 'RADIO1', default=False)
+                    sg.Radio('ИАД', 'RADIO1', default=True, key='-IAD-', disabled=False, enable_events=True),
+                    sg.Radio('не ИАД', 'RADIO1', default=False, key='-NOT IAD-', disabled=False, enable_events=True)
                 ],
             ])
         ],
@@ -46,10 +45,10 @@ def options_window() -> None:
             sg.Column([
                 [sg.Text('Рабочая директория:')],
                 [
-                    sg.InputText(default_text=DEFAULT_PATH, key='-PATH-'),
+                    sg.InputText(default_text=DEFAULT_PATH, key='-PATH-', enable_events=True),
                     sg.FolderBrowse(enable_events=True, key='-F-', target='-F-')
                 ],
-                [sg.Checkbox('сделать путём по умолчанию', key='-CHANGE DEFAULT PATH-')],
+                [sg.Checkbox('сделать путём по умолчанию', key='-CHANGE DEFAULT PATH-', enable_events=True)],
                 [sg.Checkbox('удалить существующие файлы по текущей категории', key='-WILL DELETE-',
                              enable_events=True)],
                 [sg.T('')],
@@ -60,8 +59,6 @@ def options_window() -> None:
                 [sg.Text('Загрузка данных:')],
                 [
                     sg.Radio('с сайта', 'RADIO2', default=True, key='-FROM ELIB-', enable_events=True),
-                    sg.Radio('из файла', 'RADIO2', default=False, key='-FROM FILE-',
-                             enable_events=True, disabled=False),
                     sg.Radio('из файла статей', 'RADIO2', default=False, key='-FROM FILE ARTICLES-',
                              enable_events=True, disabled=False)
                 ],
@@ -76,8 +73,11 @@ def options_window() -> None:
         ],
         [
             sg.Column([
-                [sg.Checkbox('предпросмотр', default=True, key='-ON SCREEN-', enable_events=True)],
-                [sg.Text('Количество статей в окне предпросмотра: '), sg.InputText('3', 5, key='-ARTICLES TO PREVIEW-')]
+                [sg.Checkbox('просмотр выборки', default=True, key='-ON SCREEN-', enable_events=True)],
+                [
+                    sg.Text('Показать выборку в окне предпросмотра: '),
+                    sg.InputText('3', 5, key='-ARTICLES TO PREVIEW-')
+                ]
             ])
         ],
         [sg.Text('', text_color='orange red', key='-FILE NOTIFICATION-')],
@@ -87,7 +87,7 @@ def options_window() -> None:
         ]
     ]
 
-    right_path = DEFAULT_PATH
+    correct_path = DEFAULT_PATH
     window = sg.Window('ПАД', options_layout)
 
     while True:
@@ -100,17 +100,13 @@ def options_window() -> None:
             window['-PATH-'].update(values['-F-'].replace('/', '\\'))
         elif event == '-WILL DELETE-':
             if values['-WILL DELETE-']:
-                window['-FROM FILE-'].update(disabled=True)
                 window['-FROM FILE ARTICLES-'].update(disabled=True)
                 window['-FROM ELIB-'].update(True)
                 window['-FROM ELIB MENU-'].unhide_row()
             else:
-                window['-FROM FILE-'].update(disabled=False)
                 window['-FROM FILE ARTICLES-'].update(disabled=False)
         elif event == '-FROM ELIB-':
             window['-FROM ELIB MENU-'].unhide_row()
-        elif event == '-FROM FILE-':
-            window['-FROM ELIB MENU-'].hide_row()
         elif event == '-FROM FILE ARTICLES-':
             window['-FROM ELIB MENU-'].hide_row()
         elif event == '-ON SCREEN-':
@@ -122,71 +118,78 @@ def options_window() -> None:
             folder_path = values['-PATH-']
             if not os.path.exists(folder_path):
                 sg.PopupError('Такого пути несуществует!', title='', text_color='white', keep_on_top=True)
-                window['-PATH-'].update(right_path)
+                window['-PATH-'].update(correct_path)
                 continue
             else:
-                right_path = values['-PATH-']
+                correct_path = values['-PATH-']
 
-            is_iad = values['-IS IAD-']
-            file_html = 'IAD_html.bin' if is_iad else 'NOT_IAD_html.bin'
+            is_iad = values['-IAD-']
             file_articles = 'IAD.bin' if is_iad else 'NOT_IAD.bin'
             articles_per_page = 0
-            num_of_pages = int(values['-NUM OF PAGES-'])
-            path_html_exists = os.path.exists(folder_path + '\\' + file_html)
+            num_of_pages = 0
+            num_of_articles_per_query = 0
             path_articles_exists = os.path.exists(folder_path + '\\' + file_articles)
             from_elib = values['-FROM ELIB-']
-            if not path_articles_exists and not path_html_exists and not from_elib:
-                window['-FILE NOTIFICATION-'].update('Файлы не найдены!', text_color='orange red')
+            if is_iad and not os.path.exists(folder_path + '\\' + 'IAD.bin'):
+                window['-NOT IAD-'].update()
+                window['-IAD-'].update(True)
+            if not path_articles_exists and not from_elib:
+                window['-FILE NOTIFICATION-'].update('Файл не найден!', text_color='orange red')
                 window['-FROM FILE ARTICLES-'].update(disabled=True)
-                window['-FROM FILE-'].update(disabled=True)
                 window['-FROM ELIB-'].update(True)
                 window['-FROM ELIB MENU-'].unhide_row()
                 continue
-            elif not path_articles_exists and values['-FROM FILE ARTICLES-']:
-                window['-FILE NOTIFICATION-'].update('Файл не найден!', text_color='orange red')
-                window['-FROM FILE ARTICLES-'].update(disabled=True)
-                window['-FROM FILE-'].update(disabled=False)
-                continue
-            elif not path_html_exists and values['-FROM FILE-']:
-                window['-FILE NOTIFICATION-'].update('Файл не найден!', text_color='orange red')
-                window['-FROM FILE ARTICLES-'].update(disabled=False)
-                window['-FROM FILE-'].update(disabled=True)
-                continue
             elif from_elib:
+                if values['-NOT IAD-'] and not os.path.exists(folder_path + '\\' + 'IAD.bin'):
+                    window['-NOT IAD-'].update(disabled=True)
+                    window['-IAD-'].update(True)
+                    sg.PopupError('Сначала сформируйте категорию ИАД')
+                    continue
+                else:
+                    window['-NOT IAD-'].update(True, disabled=False)
+
                 try:
                     articles_per_page = int(values['-ARTICLES PER PAGE-'])
                 except ValueError:
-                    sg.PopupError('Введите число в поле Количество статей')
+                    sg.PopupError("Введите число в поле 'Количество статей на странице'")
+                    continue
+
+                try:
+                    num_of_pages = int(values['-NUM OF PAGES-'])
+                except ValueError:
+                    sg.PopupError("Введите число в поле 'Количество страниц'")
                     continue
                 else:
-                    request_option_size = len(IAD_OPTIONS) if is_iad else len(NOT_IAD_OPTIONS)
-                    max_num_of_articles = MAX_NUM_OF_ARTICLES['IAD'] if is_iad else MAX_NUM_OF_ARTICLES['NOT IAD']
-                    num_of_articles = request_option_size * articles_per_page * num_of_pages
-                    if num_of_articles < 50 or num_of_articles > max_num_of_articles:
-                        error_text = 'Максимальное число считываемых страниц\n' + \
-                                     'для количества статей на странице = ' + str(articles_per_page) + ':\n' + \
-                                      str(int(max_num_of_articles / articles_per_page / request_option_size))
-                        sg.PopupError(error_text, title='', text_color='white', keep_on_top=True)
-                        continue
+                    if values['-ON SCREEN-']:
+                        request_option_size = len(IAD_OPTIONS) if is_iad else len(NOT_IAD_OPTIONS)
+                        max_num_of_articles = MAX_NUM_OF_ARTICLES['IAD'] if is_iad else MAX_NUM_OF_ARTICLES['NOT IAD']
+                        num_of_articles_for_download = request_option_size * articles_per_page * num_of_pages
+                        num_of_articles_per_query = articles_per_page * num_of_pages
+                        if num_of_articles_for_download < 50 or num_of_articles_for_download > max_num_of_articles:
+                            error_text = 'Максимальное число считываемых страниц\n' + \
+                                         'для количества статей на странице = ' + str(articles_per_page) + ':\n' + \
+                                         str(int(max_num_of_articles / articles_per_page / request_option_size))
+                            sg.PopupError(error_text, title='', text_color='white', keep_on_top=True)
+                            continue
 
                 if values['-WILL DELETE-']:
                     window['-WILL DELETE-'].update(False)
                 window['-FROM FILE ARTICLES-'].update(True, disabled=False)
-                window['-FROM FILE-'].update(disabled=False)
                 window['-FROM ELIB MENU-'].hide_row()
-                window['-FILE NOTIFICATION-'].update('Файлы успешно сохранены!', text_color='green2')
+                window['-FILE NOTIFICATION-'].update('Файл успешно сохранён!', text_color='green2')
 
             else:
-                window['-FILE NOTIFICATION-'].update('Файл успешно сохранён!', text_color='green2')
+                window['-FILE NOTIFICATION-'].update('Файл успешно загружен!', text_color='green2')
 
             try:
                 preview_num = int(values['-ARTICLES TO PREVIEW-'])
             except ValueError:
-                sg.PopupError('Введите число от 1 до 50', title='', text_color='white', keep_on_top=True)
+                sg.PopupError("""В поле 'Количество статей в окне предпросмотра':
+                            Введите число от 1 до 50""", title='', text_color='white', keep_on_top=True)
                 continue
             else:
                 if preview_num < 1 or preview_num > 50:
-                    sg.PopupError("""В поле Количество статей в окне предпросмотра:
+                    sg.PopupError("""В поле 'Количество статей в окне предпросмотра':
                                 Введите число от 1 до 50""", title='', text_color='white', keep_on_top=True)
                     continue
 
@@ -197,17 +200,12 @@ def options_window() -> None:
             if change_default_path:
                 set_config({'DEFAULT PATH': folder_path})
             os.chdir(folder_path)
-            if delete_binary:
-                if os.path.exists(file_html):
-                    delete_file(file_html)
-                if os.path.exists(file_articles):
-                    delete_file(file_articles)
+            if delete_binary and os.path.exists(file_articles):
+                delete_file(file_articles)
 
             if from_elib:
-                download(make_urls(is_iad, articles_per_page, num_of_pages), file_html)
-                dump_binary(file_articles, parse(file_html))
-            elif values['-FROM FILE-']:
-                dump_binary(file_articles, parse(file_html))
+                htmls = download_all_htmls(is_iad, articles_per_page, num_of_pages)
+                dump_binary(parse(is_iad, htmls, num_of_articles_per_query), file_articles)
 
             if on_screen:
                 articles_window(folder_path + '\\' + file_articles, preview_num)
